ccbcbff4045f4f87b36d786c9bb389ebd76718e7
Prof OKalu <okalu1@outlook.com>
2025-02-27 12:35:08 -0600

    Initial commit

---
+++ src/main/java/edu/miu/cs/cs425/crmapp/model/Customer.java
@@ -0,0 +1,48 @@
+package edu.miu.cs.cs425.crmapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.StringJoiner;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Customer {
+    private Integer customerId;
+    private String name;
+
+//    public Customer(Integer customerId, String name) {
+//        this.customerId = customerId;
+//        this.name = name;
+//    }
+//
+//    public Customer() {
+//        this(null, null);
+//    }
+//
+//    public Integer getCustomerId() {
+//        return customerId;
+//    }
+//
+//    public void setCustomerId(Integer customerId) {
+//        this.customerId = customerId;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return new StringJoiner(", ", Customer.class.getSimpleName() + "[", "]")
+//                .add("customerId=" + customerId)
+//                .add("name='" + name + "'")
+//                .toString();
+//    }
+}
---
+++ src/main/java/edu/miu/cs/cs425/crmapp/model/CustomerType.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs425.crmapp.model;
+
+public record CustomerType(
+        Integer customerTypeId,
+        String typeName
+) {
+}
